3186d34c771bd49afabe99c62c1c66ebebf13873
Saku Linnankoski <59973451+beetor1@users.noreply.github.com>
2021-01-06 14:17:53 +0200

    v1.2

--- randomPassword.py
+++ randomPassword.py
@@ -2,9 +2,9 @@ import random
 
 #Random Password Generator
 #author: Saku Linnankoski
-#version: v1.1
+#version: 1.2
 
-#Exepted characters
+#Accepted characters in password
 salpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 lalpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 num = ['0','1','2','3','4','5','6','7','8','9']
